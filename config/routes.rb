22531f15d25969df05759a303db70f98b8901973
marincapaul <marincapaul@outlook.com>
2023-03-30 15:36:28 +0300

    messed around with bundler

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'pages#home'
+  get :test, to: 'pages#test'
   devise_for :owners
   resources :owners
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
