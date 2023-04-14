db9609c7831124d940efc14b365cbafabc72a245
marincapaul <marincapaul@outlook.com>
2023-04-14 13:11:59 +0300

    i broke the production

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'pages#home'
-  devise_for :owners, controllers: { registration: 'owners/registration' }
+  devise_for :owners
   get :test, to: 'pages#test'
   resources :owners
 
