ef81a45b03c7ddc4daf4a8f046dea6670eb72923
marincapaul <marincapaul@outlook.com>
2023-06-27 18:35:53 +0300

    done random things

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -4,5 +4,6 @@ class Owner < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :pets
   has_one_attached :avatar
 end
