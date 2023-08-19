23aba28c2c6d34e035d0552427bea24fb784b6a6
marincapaul <marincapaul@outlook.com>
2023-08-19 12:39:10 +0300

    add Ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @post.pet, notice: 'Comment was successfully created.' } # changed the redirect to @post
+        format.js
       else 
         format.html { redirect_to @post.pet, notice: 'There was a problem posting your comment.' } # changed the redirect to @post
       end
